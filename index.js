d1317de71d79f6be9f539408241c1295713fefe9
dreuxGab <dev.dreux@hotmail.com>
2024-08-09 16:22:49 -0300

    token formato req body

--- index.js
+++ index.js
@@ -76,10 +76,11 @@ app.post('/proxy-mercado-pago-pix', async (req, res) => {
           'X-Idempotency-Key': req.headers['x-idempotency-key'],
         },
       });
-      
+      console.log('Received Request Body PIX:', JSON.stringify(req.body, null, 2));
+      console.log('Response from Mercado Pago API:', response.data);
       res.status(response.status).json(response.data);
     } catch (error) {
-    //   console.error('Error making API request:', error.message);
+      console.error('Error making API request:', error.message);
       res.status(error.response ? error.response.status : 500).json({
         message: error.message,
       });
@@ -89,44 +90,100 @@ app.post('/proxy-mercado-pago-pix', async (req, res) => {
 
 
 
+// app.post('/proxy-card-to-token', async (req, res) => {
+//     try {
+//       const { cardNumber, cardholderName, cardExpirationMonth, cardExpirationYear, securityCode, identificationType, identificationNumber } = req.body;
+//       const publicKey = req.query.publicKey;
+//       const accessToken = req.headers['authorization'];
+  
+//       // Configuração da requisição para a API do Mercado Pago
+//       const response = await axios.post(
+//         `https://api.mercadopago.com/v1/card_tokens?public_key=${publicKey}`,
+//         {
+//           card_number: cardNumber,
+//           cardholder: {
+//             name: cardholderName,
+//             identification: {
+//               type: identificationType || 'CPF',
+//               number: identificationNumber
+//             }
+//           },
+//           security_code: securityCode,
+//           expiration_month: cardExpirationMonth,
+//           expiration_year: cardExpirationYear
+//         },
+//         {
+//           headers: {
+//             'Authorization': accessToken,
+//             'Content-Type': 'application/json'
+//           }
+//         }
+//       );
+  
+//     //   console.log('Response from CARD TO TOKEN:', response.data);
+//       // Retornar a resposta da API para o cliente
+//       res.json(response.data);
+//     } catch (error) {
+//     //   console.error(error);
+//       res.status(error.response ? error.response.status : 500).json({ error: error.message });
+//     }
+// });
+
+
+
+
+
 app.post('/proxy-card-to-token', async (req, res) => {
-    try {
-      const { cardNumber, cardholderName, cardExpirationMonth, cardExpirationYear, securityCode, identificationType, identificationNumber } = req.body;
-      const publicKey = req.query.publicKey;
-      const accessToken = req.headers['authorization'];
-  
-      // Configuração da requisição para a API do Mercado Pago
-      const response = await axios.post(
-        `https://api.mercadopago.com/v1/card_tokens?public_key=${publicKey}`,
-        {
-          card_number: cardNumber,
-          cardholder: {
-            name: cardholderName,
-            identification: {
-              type: identificationType || 'CPF',
-              number: identificationNumber
-            }
-          },
-          security_code: securityCode,
-          expiration_month: cardExpirationMonth,
-          expiration_year: cardExpirationYear
-        },
-        {
-          headers: {
-            'Authorization': accessToken,
-            'Content-Type': 'application/json'
-          }
-        }
-      );
-  
-    //   console.log('Response from CARD TO TOKEN:', response.data);
-      // Retornar a resposta da API para o cliente
-      res.json(response.data);
-    } catch (error) {
-    //   console.error(error);
-      res.status(error.response ? error.response.status : 500).json({ error: error.message });
+  try {
+    // Obtenha o corpo da requisição
+    const { card_number, cardholder, security_code, expiration_month, expiration_year } = req.body;
+    console.log("BODY REQ TOKEN: ", req.body);
+    // Obtenha a chave pública da URL
+    const publicKey = req.query.public_key;
+    if (!publicKey) {
+      return res.status(400).json({ error: 'Public key is required' });
     }
+
+    // Obtenha o token de acesso dos headers
+    const accessToken = req.headers['authorization'];
+    if (!accessToken) {
+      return res.status(400).json({ error: 'Authorization header is required' });
+    }
+
+    // Prepare o payload para a chamada à API do Mercado Pago
+    const payload = {
+      card_number,
+      cardholder,
+      security_code,
+      expiration_month,
+      expiration_year
+    };
+
+    // Faça a chamada para a API do Mercado Pago
+    const response = await axios.post(`https://api.mercadopago.com/v1/card_tokens?public_key=${publicKey}`, payload, {
+      headers: {
+        'Authorization': accessToken,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // Envie a resposta da API do Mercado Pago de volta para o cliente
+    res.json(response.data);
+    console.log('RESPOSTA TOKEN:', response.data);
+
+  } catch (error) {
+    // Em caso de erro, envie uma resposta de erro
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+
+
+
+
+
+
+
 
 
 
@@ -142,7 +199,7 @@ app.post('/proxy-mercado-pago-cc', async (req, res) => {
     // Corpo da requisição que será enviado para a API do Mercado Pago
     const apiRequestBody = req.body;
 
-    // console.log('Received Request Body:', JSON.stringify(apiRequestBody, null, 2));
+    console.log('Received Request Body:', JSON.stringify(apiRequestBody, null, 2));
   
     try {
       const response = await axios.post('https://api.mercadopago.com/v1/payments', apiRequestBody, {
@@ -152,11 +209,11 @@ app.post('/proxy-mercado-pago-cc', async (req, res) => {
         },
       });
   
-    // console.log('Response from Mercado Pago API:', response.data);
+    console.log('Response from Mercado Pago API:', response.data);
       // Enviar a resposta da API do Mercado Pago de volta para o cliente
       res.status(response.status).json(response.data);
     } catch (error) {
-    //   console.error('Error making API call:', error.response ? error.response.data : error.message);
+      console.error('Error making API call:', error.response ? error.response.data : error.message);
       res.status(error.response ? error.response.status : 500).json(error.response ? error.response.data : { error: 'Internal Server Error' });
     }
 });
